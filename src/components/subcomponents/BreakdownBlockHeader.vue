1f1d2b6cddca8c283e7c814f50646e7cd7d16f77
sally358 <superinfinitesallytm@gmail.com>
2025-10-01 11:43:41 +0200

    Create BreakdownBlockHeader.vue

---
+++ src/components/subcomponents/BreakdownBlockHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex flex-col w-full shrink-0 h-12 border-b border-gray-500">
+        <div class="flex h-full w-full px-4 items-center text-lg">
+             {{ label }} ({{ number }} combos)
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+  const props = defineProps<{
+    label: string,
+    number: number
+  }>();
+
+
+</script>
