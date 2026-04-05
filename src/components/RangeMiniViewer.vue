9c19571890c5a18c10708b9fed854ecfb7befc2c
sally358 <superinfinitesallytm@gmail.com>
2026-04-05 17:28:08 +0200

    Refactor background style logic in RangeMiniViewer
    Finger painting

--- src/components/RangeMiniViewer.vue
+++ src/components/RangeMiniViewer.vue
@@ -15,8 +15,8 @@
           <div
             class="absolute w-full h-full left-0 top-0 bg-bottom bg-no-repeat"
             :style="{
-              'background-image': `linear-gradient(${yellow500} 0% 100%)`,
-              'background-size': `100% ${cellValue(row, col)}%`,
+              'background-image': styleParser(row, col)[0],
+              'background-size': styleParser(row, col)[1]
             }"
           ></div>
         </div>
@@ -28,7 +28,17 @@
 <script setup lang="ts">
 import { useStore } from "../store";
 
+
 const yellow500 = "#eab308";
+const green500 = "#22c55e";
+const red500 = "#ef4444";
+
+const colorPack: string[] = [] 
+
+colorPack[1] = green500
+colorPack[0] = yellow500
+colorPack[-1] = red500
+
 
 const props = defineProps<{ player: number }>();
 const store = useStore();
@@ -37,4 +47,46 @@ const cellValue = (row: number, col: number) => {
   const cellIndex = (row - 1) * 13 + (col - 1);
   return store.ranges[props.player][cellIndex];
 };
+
+const cellLimit = (row: number, col: number) => {
+  const cellIndex = (row - 1) * 13 + (col - 1);
+  return store.currentLimitRange[cellIndex];
+};
+
+const styleParserNodelock = (row: number, col: number) => {
+  const weight = cellValue(row, col);
+  const limit = cellLimit(row, col);
+
+  if (limit != 1) 
+  {
+    // finger painting support section
+    
+    if (weight == 0)
+      if (col == row)
+        return ["linear-gradient(#270067 0% 100%)", "100% 100%"];
+      else
+        return ["linear-gradient(#270047 0% 100%)", "100% 100%"];
+    else
+      if (col == row)
+        return ["linear-gradient(#270067 0% " + (100 - weight) + "%, " + colorPack[limit] + " " + (100 - weight) + "% 100%)", "100% 100%"];
+      else
+        return ["linear-gradient(#270047 0% " + (100 - weight) + "%, " + colorPack[limit] + " " + (100 - weight) + "% 100%)", "100% 100%"];
+  }
+  else
+    return ["linear-gradient(" + colorPack[limit] + " 0% 100%)", "100% " + weight + "%"];
+}
+
+const styleParserNormal = (row: number, col: number) => {
+  const weight = cellValue(row, col);
+  return ["linear-gradient(" + colorPack[0] + " 0% 100%)", "100% " + weight + "%"];
+}
+
+let styleParser: (row: number, col: number) => string[];
+
+if (props.player === 2) {
+  styleParser = styleParserNodelock;
+} else {
+  styleParser = styleParserNormal;
+}
+
 </script>
