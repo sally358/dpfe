31d35cfb42b1cffea0f1a3f0e27ef6372ab911c9
sally358 <superinfinitesallytm@gmail.com>
2026-07-18 22:05:27 +0200

    deglitching and mandatory lock checks

--- src/components/RunSolver.vue
+++ src/components/RunSolver.vue
@@ -437,10 +437,7 @@ const checkConfig = (
   if (
     ![0, 3, 4, 5].includes(config.expectedBoardLength) ||
     (config.expectedBoardLength === 0 &&
-      (addedLinesArray.length > 0 || removedLinesArray.length > 0)) ||
-    (config.expectedBoardLength > 0 &&
-      addedLinesArray.length === 0 &&
-      removedLinesArray.length === 0)
+      (addedLinesArray.length > 0 || removedLinesArray.length > 0))
   ) {
     return "Invalid configurations (loaded broken configurations?)";
   }
@@ -596,6 +593,17 @@ const buildTree = async () => {
   if (errorString) {
     isTreeBuilding.value = false;
     treeStatus.value = "Error: " + errorString;
+    return;
+  }
+
+  treeStatus.value = "Verifying nodelocks..."
+
+  const isError = await invokes.gameVerifyLocks();
+
+  if (isError)
+  {
+    isTreeBuilding.value = false;
+    treeStatus.value = "Error: Conflicting nodelocks. Consult LockSheriff™ in Nodes menu for more info."
     return;
   }
 
